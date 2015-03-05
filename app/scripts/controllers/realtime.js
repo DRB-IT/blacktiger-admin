72a5e189020b41f088c9d673d1f9d94d3b74b70a
Michael Krog <mic@apaq.dk>
2015-03-05 12:19:12 +0100

    Fixed #9.

--- app/scripts/controllers/realtime.js
+++ app/scripts/controllers/realtime.js
@@ -13,15 +13,77 @@ angular.module('blacktiger-controllers')
             $scope.data = {};
 
             $scope.buildData = function () {
-                $scope.data.noOfRooms = MeetingSvc.getTotalRooms();
-                $scope.data.noOfParticipants = MeetingSvc.getTotalParticipants();
-                $scope.data.noOfCommentRequests = MeetingSvc.getTotalParticipantsByCommentRequested(true);
-                $scope.data.noOfOpenMicrophones = MeetingSvc.getTotalParticipantsByMuted(false);
+                var roomIds = MeetingSvc.findAllIds();
+                $scope.data.noOfRooms = 0;
+                $scope.data.noOfParticipants = 0;
+                $scope.data.noOfCommentRequests = 0;
+                $scope.data.noOfOpenMicrophones = 0;
+                var sipCalls = 0;
+                
+                angular.forEach(MeetingSvc.findAllIds(), function(id) {
+                   var room = MeetingSvc.findRoom(id);
+                   if(!$scope.areaCode || room.countryCallingCode === $scope.areaCode) {
+                       $scope.data.noOfRooms++;
+                       angular.forEach(room.participants, function(participant) {
+                          if(participant.host === false) {
+                              $scope.data.noOfParticipants++;
+                              
+                              if(!participant.muted) {
+                                  $scope.data.noOfOpenMicrophones++;
+                              }
+                              
+                              if(participant.commentRequested) {
+                                  $scope.data.noOfCommentRequests++;
+                              }
+                              
+                              if(participant.type === 'Sip') {
+                                  sipCalls++;
+                              }
+                          } 
+                       });
+                   }
+                });
                 $scope.data.noOfParticipantsPerRoom = $scope.data.noOfRooms === 0 || $scope.data.noOfParticipants === 0 ? 0 : $scope.data.noOfParticipants / $scope.data.noOfRooms;
 
-                var count = MeetingSvc.getTotalParticipantsByType('Sip');
-                $scope.data.sipPercentage = count === 0 ? 0.0 : (count / $scope.data.noOfParticipants) * 100;
+                $scope.data.sipPercentage = sipCalls === 0 ? 0.0 : (sipCalls / $scope.data.noOfParticipants) * 100;
                 $scope.data.phonePercentage = $scope.data.noOfParticipants === 0 ? 0.0 : 100 - $scope.data.sipPercentage;
+            };
+
+            $scope.resolveActiveAreas = function() {
+                var areas = ['All'];
+                
+                angular.forEach(MeetingSvc.findAllIds(), function(id) {
+                   var room = MeetingSvc.findRoom(id);
+                   if(areas.indexOf(room.countryCallingCode)<0) {
+                       areas.push(room.countryCallingCode);
+                   }
+               });
+               return areas;
+            };
+            
+            $scope.selectArea = function ($event) {
+                $mdDialog.show({
+                    targetEvent: $event,
+                    templateUrl: 'views/dialog-select-area.html',
+                    locals: {
+                        items: $scope.resolveActiveAreas()
+                    },
+                    controller: DialogController
+                }).then(function(result) {
+                    $scope.areaCode = result !== 'All' ? result : undefined;
+                    $scope.buildData();
+                });
+                function DialogController(scope, $mdDialog, items) {
+                    scope.items = items;
+                    
+                    scope.select = function(item) {
+                        $mdDialog.hide(item);
+                    };
+                    
+                    scope.closeDialog = function () {
+                        $mdDialog.hide();
+                    };
+                }
             };
 
             $scope.reload = function (event) {
@@ -31,7 +93,7 @@ angular.module('blacktiger-controllers')
                         .content('This is not yet implemented')
                         .ok('Close')
                         .targetEvent(event)
-                );
+                        );
             };
 
             $scope.updateSystemInfo = function () {
@@ -41,7 +103,7 @@ angular.module('blacktiger-controllers')
             };
 
             $scope.$on('$destroy', function () {
-                $timeout.cancel($scope.systemInfoTimerPromise);
+                $interval.cancel($scope.systemInfoTimerPromise);
             });
 
             /** WATCHES START **/
@@ -55,6 +117,6 @@ angular.module('blacktiger-controllers')
             }, $scope.buildData)
             /** WATCHES END **/
 
-            $interval($scope.updateSystemInfo, 5000);
+            $scope.systemInfoTimerPromise = $interval($scope.updateSystemInfo, 5000);
             $scope.updateSystemInfo();
         });
