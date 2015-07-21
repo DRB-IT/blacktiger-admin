5978d8ca51fbb5e170d743b8bb98d5b9f04d92af
Michael Krog <mic@apaq.dk>
2015-07-21 10:59:51 +0200

    Added missing files related to #

---
+++ app/scripts/controllers/hall-edit.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blacktiger.controllers:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the blacktiger-admin interface
+ */
+angular.module('blacktiger-controllers')
+        .controller('HallEditCtrl', function ($scope, RoomSvc, $routeParams, $window) {
+            RoomSvc.get($routeParams.id).$promise.then(function(hall) {
+                $scope.hall = hall;
+                $scope.orgHall = angular.copy(hall);
+            });
+
+            $scope.isChanged = function() {
+                return !angular.equals($scope.hall, $scope.orgHall);
+            }
+
+            $scope.save = function() {
+                RoomSvc.save($scope.hall).$promise.then(function() {
+                    $window.history.back();
+                });
+            }
+        });
